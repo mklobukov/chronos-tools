f7ecc84195219901cddc9242190f529f039c1323
Mark <mark_klobukov@comcast.com>
2017-09-01 12:23:04 -0400

    Added login command

---
+++ login_cmd.js
@@ -0,0 +1,72 @@
+const utils = require('./utils');
+const prompt = require('prompt');
+const colors = require('colors/safe');
+const fs = require('fs')
+
+module.exports = function(key, secret, credPath, credFileName, authServerURL) {
+  this.promptAndSaveCredentials(key, secret, credPath, credFileName)
+  .then(response => {
+    const appKey = response["appKey"];
+    const appSecret = response["appSecret"];
+    authString = appKey + ":" + appSecret;
+    const buffer = new Buffer(authString);
+    const base64Auth = buffer.toString('base64');
+    utils.getToken(authServerURL, base64Auth)
+    .then(response => {
+      console.log("Token: ", response["Token"])
+    })
+    .catch(error => {
+      console.log("Could not get token with provided credentials");
+    })
+  })
+  .catch(error => {
+    console.log("Error: ", error);
+  })
+}
+
+promptAndSaveCredentials = function(key, secret, credPath, credFileName) {
+  return new Promise(function(fulfill, reject) {
+    let appKey = key;
+    let appSecret = secret;
+    const credDirPath = process.env.HOME + "/" + credPath;
+    const credFilePath = credDirPath + credFileName;
+
+    if (appKey != "" && appSecret != "") {
+      return fulfill({"appKey" : appKey, "appSecret" : appSecret})
+    }
+
+    prompt.message = colors.magenta("")
+    prompt.delimiter = colors.green(":")
+    prompt.start();
+    prompt.get({
+      properties: {
+        appkey: {
+          description: colors.magenta("App key")
+        },
+        appsecret: {
+          description: colors.cyan("App secret")
+        }
+      }}, function (err, result) {
+          appKey = result.appkey.replace(/(\r\n|\n|\r)/gm,"");
+          appSecret = result.appsecret.replace(/(\r\n|\n|\r)/gm,"");
+          console.log(appKey, appSecret)
+          //write credentials to the file
+          //if the directory with credentials file doesn't exist, create it
+          if (!fs.existsSync(credDirPath)) {
+            fs.mkdirSync(credDirPath);
+          }
+
+          //write appkey and appsecret to the file inside this directory
+          const dataStr = `{"key": "${appKey}", "secret": "${appSecret}"}`
+          console.log("Data string: ", dataStr)
+          fs.writeFile(credFilePath, dataStr, function(err) {
+            if (err) {
+              console.log("Error writing credentials to file")
+              return reject(err)
+            }
+            return fulfill({"appKey" : appKey, "appSecret" : appSecret})
+          })
+        }
+    );
+  })
+}
--- main.js
+++ main.js
@@ -3,22 +3,27 @@ const getAllJobsInfo = require('./all_info_cmd')
 const getJobInfo = require('./info_cmd')
 const removeJob = require('./remove_cmd')
 const publish = require('./publish_cmd')
+const login = require('./login_cmd')
 const Config = require('./config')
 const version = require('./version_cmd')
 const schedule = require('./schedule_cmd')
-const utils = require('./utils');
+const utils = require('./utils')
 
 //argv[0] -- node binary path
 //argv[1] -- current directory
 var cmd = process.argv[2];
-var arg = "";
+var arg1 = "";
+var arg2 = "";
 if (process.argv[3]) {
-  arg = process.argv[3];
+  arg1 = process.argv[3];
+}
+if (process.argv[4]) {
+  arg2 = process.argv[4]
 }
 
 switch(cmd) {
   case "login":
-  console.log(process.env.HOME)
+    login(arg1, arg2, Config.credPath, Config.credFileName, Config.authServerURL);
     break;
 
   case "publish":
@@ -26,15 +31,15 @@ switch(cmd) {
     break;
 
   case "schedule":
-    schedule(Config.chronosFullURL, arg, Config.credPath, Config.credFileName, Config.authServerURL);
+    schedule(Config.chronosFullURL, arg1, Config.credPath, Config.credFileName, Config.authServerURL);
     break;
 
   case "remove":
-    removeJob(Config.chronosFullURL, Config.credPath, Config.credFileName, Config.authServerURL, arg);
+    removeJob(Config.chronosFullURL, Config.credPath, Config.credFileName, Config.authServerURL, arg1);
     break;
 
   case "info":
-    getJobInfo(Config.credPath, Config.credFileName, Config.authServerURL, Config.chronosFullURL, arg);
+    getJobInfo(Config.credPath, Config.credFileName, Config.authServerURL, Config.chronosFullURL, arg1);
     break;
 
   case "allinfo":
